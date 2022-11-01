6b57893f89ed1c364825469e5464a59daeaa4844
graphorismo <graphorismo@gmail.com>
2022-11-01 11:36:43 +0300

    Design the base of products activity

---
+++ app/src/main/java/ru/graphorismo/coffeeshop/ui/products/ProductsActivity.kt
@@ -0,0 +1,12 @@
+package ru.graphorismo.coffeeshop.ui.products
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.graphorismo.coffeeshop.R
+
+class ProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
