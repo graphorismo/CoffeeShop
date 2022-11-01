fc882cd0ce0b229010335bcaf4c06eb00908cea3
graphorismo <graphorismo@gmail.com>
2022-11-01 12:31:15 +0300

    Add a cart activity with a basic design. Fix login activity orientation to portrait. Add products activity into manifest.

---
+++ app/src/main/java/ru/graphorismo/coffeeshop/ui/cart/CartActivity.kt
@@ -0,0 +1,12 @@
+package ru.graphorismo.coffeeshop.ui.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.graphorismo.coffeeshop.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
